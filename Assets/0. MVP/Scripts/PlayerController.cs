f4e6119b38c68e5f14967c63a6346c651c31c01a
Albytrosss <AlbatrossGames@pm.me>
2025-10-04 20:54:55 +1000

    Updated JetBrains plugin

---
+++ Assets/0. MVP/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log("test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
